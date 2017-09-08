80588c308adb314856a6b069a369baec9576f28c
tonyzzp <tonyzzp@qq.com>
2017-09-08 18:02:21 +0800

    kotling

---
+++ app/src/main/java/me/izzp/chacha/kits.kt
@@ -0,0 +1,5 @@
+package me.izzp.chacha
+
+/**
+ * Created by zzp on 2017-09-08.
+ */
---
+++ app/src/test/java/me/izzp/chacha/test.kt
@@ -0,0 +1,16 @@
+package me.izzp.chacha
+
+import org.junit.Test
+
+/**
+ * Created by zzp on 2017-09-08.
+ */
+
+class TestProto {
+    @Test
+    fun test_proto() {
+//        val l = Account.Login.newBuilder().setUsername("zzp").setPassword("123").build()
+//        println(l)
+//        println(l.toByteArray())
+    }
+}
